c2082ea06207f9060cd2a4d535f961b767e28f73
Joao Queiroz <joao.queiroz.code@gmail.com>
2022-10-17 20:38:38 -0300

    Mudando cor de fundo e containers

--- api.js
+++ api.js
@@ -1,11 +1,13 @@
 function adviceGenerator() {
     
     let btnAdvice = document.getElementById('btnAdvice');
+    btnAdvice.innerHTML = `<lottie-player src="https://assets5.lottiefiles.com/packages/lf20_20lywwls.json"  background="transparent"  speed="1"  style="width: 3em; height: 3em;"  loop  autoplay></lottie-player>`
 
     fetch('https://api.adviceslip.com/advice').then((data) => {
         data.json().then((resp) => {
             resp = resp.slip;
             
+            btnAdvice.innerHTML = `New advice`;
             let campoConselho = document.getElementById('campoConselho');
             campoConselho.innerHTML = `${resp.advice}`;
         })
